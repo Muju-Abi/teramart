d1b2fff1707c7a91eb3557c7308bd2b03335bf62
Mohamed Mustafa <mohamedmustafa.mak@gmail.com>
2026-02-28 08:32:53 +0530

    Adding Carts page

--- src/app/carts/[slug]/page.tsx
+++ src/app/carts/[slug]/page.tsx
@@ -1,3 +1,10 @@
+import { getStoryblokApi } from "@storyblok/react/rsc";
+
+const fetchCartPage = async (slug: string) => {
+  const client = getStoryblokApi();
+  const response = await client.getStory(slug, {});
+}
+
 const CartPage = () => {
   return <div>Cart Page</div>;
 };
