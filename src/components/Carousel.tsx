4225a1a85a42c39e31c61e41ce522ce436788249
Mohamed Mustafa <mohamedmustafa.mak@gmail.com>
2026-03-06 15:23:03 +0530

    Carousel updates

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -4,13 +4,6 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 // 1. Add Navigation and Pagination to imports
 import { EffectCoverflow, Navigation, Pagination, Autoplay } from 'swiper/modules'; 
 
-// import 'swiper/css';
-// import 'swiper/css/effect-coverflow';
-// import 'swiper/css/autoplay'; 
-// 2. Add specific CSS for navigation and dots
-// import 'swiper/css/navigation'; 
-// import 'swiper/css/pagination'; 
-
 export const Carousel = ({ blok }: { blok: any }) => {
   return (
     <section {...storyblokEditable(blok)} className="py-12 relative px-10">
@@ -19,6 +12,7 @@ export const Carousel = ({ blok }: { blok: any }) => {
         effect="coverflow"
         centeredSlides={true}
         slidesPerView="auto"
+        // slidesPerView={2}
         loop={true}
         // 3. Enable Navigation (Arrows)
         navigation={true} 
